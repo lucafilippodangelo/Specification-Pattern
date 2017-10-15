a484d785520ffbda1fd4f8b2a3dffab791d12984
lucafilippodangelo <info@lucadangelo.it>
2017-10-15 18:40:00 +0100

    first

---
+++ SpecificationPatternDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpecificationPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
